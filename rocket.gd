b7e6020148f0af3be5e32e4f95eaf621da705f8f
Magnus Bakke <eucmpau001@eucnordvest.dk>
2025-01-29 10:38:10 +0100

    HUD update, fuel feature, gameover feature, score

--- rocket.gd
+++ rocket.gd
@@ -1,50 +1,86 @@
 extends RigidBody2D
 
 # Movement constants
-const MAX_ROTATION_SPEED = 15
+const MAX_ROTATION_SPEED = 18
 const ROTATION_SPEED = .008
-const MAX_THRUST = 2.0 * 100
+const MAX_THRUST = 3.0 * 100
 const THRUST_INCREMENT = 0.25 * 100
+
 
 # Variables for movement
 var current_thrust: float = 0.0
 var current_rotation: float = 0.0
-var thrust_vector = Vector2(0,-1).rotated(rotation) * current_thrust
 
 # Define a spawn position variable
 var spawn_position: Vector2 = Vector2(0, -10 * 100)  # Replace with your desired coordinates
 
-# Velocity before collision
+# Variables for landing
 var pre_collision_velocity : Vector2
+var did_land : bool = false
+var can_control : bool = true
+
+# Variable for fuel
+var fuel_remaining : int = 100
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Timer.start()
 	position = spawn_position
+	print(pre_collision_velocity)
+	$Crewmate.visible = true
+	$DeadCrewmate.visible = false
+	$CrewmateCollisionShape.disabled = true
+	$DeadCrewmateCollisionShape.disabled = false
+
 
 # Called when the rocket hits the moons surface
 func _on_body_entered(body: Node) -> void:
-	var message = str(pre_collision_velocity)
-	print (message)
+	if !did_land:
+		var message = str(pre_collision_velocity/100)
+		print (message)
+		did_land = true
+		can_control = false
+		current_thrust = 0
+
+	if pre_collision_velocity.length()/100 >= 2:
+		print(pre_collision_velocity.length())
+		$Crewmate.visible = false
+		$DeadCrewmate.visible = true
+		$CrewmateCollisionShape.disabled = true
+		$DeadCrewmateCollisionShape.disabled = false
+
+func _on_timer_timeout() -> void:
+	if fuel_remaining >= 0:
+		fuel_remaining -= current_thrust / THRUST_INCREMENT * 0.15
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	pre_collision_velocity = linear_velocity
 	
-	if Input.is_action_pressed("rotate_ccw"):
-		if angular_velocity > - MAX_ROTATION_SPEED:
-			angular_velocity -= ROTATION_SPEED
+	if !did_land:
+		pre_collision_velocity = linear_velocity
 	
-	if Input.is_action_pressed("rotate_cw"):
-		if angular_velocity < MAX_ROTATION_SPEED:
-			angular_velocity += ROTATION_SPEED
+	if fuel_remaining <= 1:
+		can_control = false
+		fuel_remaining = 0
+		current_thrust = 0
 	
-	if Input.is_action_just_pressed("increase_thrust"):
-		if current_thrust < MAX_THRUST:
-			current_thrust += THRUST_INCREMENT
-	
-	if Input.is_action_just_pressed("decrease_thrust"):
-		if current_thrust > 0:
-			current_thrust -= THRUST_INCREMENT
+	if can_control:
+		if Input.is_action_pressed("rotate_ccw"):
+			if angular_velocity > - MAX_ROTATION_SPEED:
+				angular_velocity -= ROTATION_SPEED
+		
+		if Input.is_action_pressed("rotate_cw"):
+			if angular_velocity < MAX_ROTATION_SPEED:
+				angular_velocity += ROTATION_SPEED
+		
+		if Input.is_action_just_pressed("increase_thrust"):
+			if current_thrust < MAX_THRUST:
+				current_thrust += THRUST_INCREMENT
+		
+		if Input.is_action_just_pressed("decrease_thrust"):
+			if current_thrust > 0:
+				current_thrust -= THRUST_INCREMENT
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	
--- simulation.gd
+++ simulation.gd
@@ -2,13 +2,54 @@ extends Node2D
 
 # 1 meter = 100 px
 
+#Main components
+@onready var rocket: RigidBody2D = $Rocket
+@onready var hud: CanvasLayer = $HUD
+@onready var gameover_screen: CanvasLayer = $GameoverScreen
+
+# HUD labels
 @onready var position_value: Label = $HUD/VBoxContainer/HBoxContainer/PositionValue
 @onready var thrust_value: Label = $HUD/VBoxContainer/HBoxContainer2/ThrustValue
-@onready var velocity_value: Label = $HUD/VBoxContainer/HBoxContainer3/VelocityValue
-@onready var rocket: RigidBody2D = $Rocket
+@onready var vertical_velocity_value: Label = $HUD/VBoxContainer/HBoxContainer3/VerticalVelocityValue
+@onready var horizontal_velocity_value: Label = $HUD/VBoxContainer/HBoxContainer5/HorizontalVelocityValue
+@onready var angle_value: Label = $HUD/VBoxContainer/HBoxContainer4/AngleValue
+@onready var fuel_value: Label = $HUD/VBoxContainer/HBoxContainer6/FuelValue
+
+#End screen labels
+@onready var collision_velocity_value: Label = $GameoverScreen/Panel/VBoxContainer/HBoxContainer/CollisionVelocityValue
+@onready var fuel_remaining_value: Label = $GameoverScreen/Panel/VBoxContainer/HBoxContainer2/FuelRemainingValue
+@onready var score_value: Label = $GameoverScreen/Panel/VBoxContainer/HBoxContainer3/ScoreValue
+@onready var highscore_value: Label = $GameoverScreen/Panel/VBoxContainer/HBoxContainer4/HighscoreValue
+
+
+func score():
+	var total_collision_velocity = rocket.pre_collision_velocity.length()/100
+	
+	# Constants for calculating the score
+	const k=6.25
+	const t=0.3
+	
+	if total_collision_velocity > 3:
+		return 0
+	return (100-total_collision_velocity*k)+(rocket.fuel_remaining*t)
+
+func _ready() -> void:
+	gameover_screen.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position_value.text = str(rocket.position.y*-1/100)
-	thrust_value.text = str(rocket.current_thrust/100)
-	velocity_value.text = str(rocket.linear_velocity)
+
+	position_value.text = str(snapped(rocket.position.y*-1/100,0.1))
+	thrust_value.text = str(snapped(rocket.current_thrust/100,0.1))
+	vertical_velocity_value.text = str(snapped(rocket.linear_velocity.y/100,0.1))
+	horizontal_velocity_value.text = str(snapped(rocket.linear_velocity.x/100,0.1))
+	angle_value.text = str(snapped(rocket.rotation*180/PI,0.1))
+	fuel_value.text = str(rocket.fuel_remaining)
+	
+	if rocket.did_land:
+		collision_velocity_value.text = str(snapped(rocket.pre_collision_velocity.length()/100,0.1))
+		fuel_remaining_value.text = str(rocket.fuel_remaining)
+		score_value.text = str(snapped(score(),0.1))
+		highscore_value.text = "na"
+		await get_tree().create_timer(2.0).timeout
+		gameover_screen.show()
